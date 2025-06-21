34a8b61d42b4527d0e092223a674a0fa3eac46a4
yasminlyya <93663827+YasminUlayya@users.noreply.github.com>
2025-06-21 12:06:13 +0700

    Update param_final_gui_model.py

--- param_final_gui_model.py
+++ param_final_gui_model.py
@@ -784,6 +784,20 @@ def main_app():
     selected_page = st.sidebar.radio("Menu", list(pages.keys()))
     pages[selected_page]()
 
+    with st.sidebar:
+        selected = option_menu(
+            menu_title="Main Menu",
+            options=list(pages.keys()),
+            icons=["cloud-upload", "cpu", "bar-chart", "clipboard-data", "graph-up"],
+            menu_icon="cast",
+            default_index=0,
+            styles={
+                "container": {"padding": "5px", "background-color": "#f8f9fa"},
+                "icon": {"color": "orange", "font-size": "14px"}, 
+                "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+                "nav-link-selected": {"background-color": "#0d6efd"},
+            }
+        )
 def main():
     initialize_session_state()  # Panggil inisialisasi di awal
 
